80cc5ed33a2ebbd8b3d3f25a6341f9f982b1f9af
Ahmed Anis <anis@Ahmeds-MacBook-Air.local>
2026-05-03 21:30:18 +0300

    feat: add ConnectivityService for offline detection

---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/foundation.dart';
+
+class ConnectivityService extends ChangeNotifier {
+  bool _isOnline = true;
+  StreamSubscription<List<ConnectivityResult>>? _sub;
+
+  bool get isOnline => _isOnline;
+
+  ConnectivityService() {
+    _init();
+  }
+
+  Future<void> _init() async {
+    final results = await Connectivity().checkConnectivity();
+    _isOnline = _hasConnection(results);
+    _sub = Connectivity().onConnectivityChanged.listen((results) {
+      final online = _hasConnection(results);
+      if (online != _isOnline) {
+        _isOnline = online;
+        notifyListeners();
+      }
+    });
+  }
+
+  bool _hasConnection(List<ConnectivityResult> results) =>
+      results.any((r) => r != ConnectivityResult.none);
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+}
